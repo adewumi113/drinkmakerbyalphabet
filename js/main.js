05d92eff558c7d1a5c9f71c4d24fcfe210800564
Afeez Adewumi <adewumilanre01@gmail.com>
2023-02-07 19:29:24 -0600

    modified html and js file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+let intervalId;
+
 document.querySelector("button").addEventListener('click', getDrink)
 
 function getDrink() {
+      document.querySelector('h2').innerText = '';
+      document.querySelector('img').src = '';
+      document.querySelector('#english').innerText = '';
+      document.querySelector('#german').innerText = '';
+      document.querySelector('#italian').innerText = '';
+      clearInterval(intervalId);
+
   let drinkByAlphabet = document.querySelector('input').value
 
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${drinkByAlphabet}`)
@@ -10,7 +19,7 @@ function getDrink() {
 
       console.log(data.drinks) 
       let i = 0;
-      const intervalId = setInterval(() => {
+      intervalId = setInterval(() => {
       // Set the slide stuff here with document.querySelectors
       document.querySelector('h2').innerText = data.drinks[i].strDrink
       document.querySelector('img').src = data.drinks[i].strDrinkThumb 
@@ -18,7 +27,7 @@ function getDrink() {
       document.querySelector('#german').innerText = data.drinks[i].strInstructionsDE
       document.querySelector('#italian').innerText = data.drinks[i].strInstructionsIT
       i = i === data.drinks.length-1 ? 0 : i+1;
-      }, 9000);
+      }, 7000);
       setTimeout(() => { clearInterval(intervalId); alert('stop making drinks'); }, 375000);
       
     })
